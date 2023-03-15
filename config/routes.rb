bc9c27760748c17c13b0b8b2f365c4fcdc70154a
Preston <prestonosoro56@gmail.com>
2023-03-16 02:42:54 +0300

    completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :plants, only: [:index, :show, :create]
+  #PATCH /plants/:id
+  patch '/plants/:id', to: 'plants#update'
+  delete '/plants/:id', to: 'plants#destroy'
 end
